bc80a5e5b81f445a4af7c6b5616ede7809102182
Tom <tomangioletti@gmail.com>
2024-12-04 20:52:37 -0500

    Day 4 part 1 complete.

--- tom2024/4-1.js
+++ tom2024/4-1.js
@@ -4,7 +4,6 @@ const IN=require('node:fs')
   .split('\n')
   .filter(x=>x.trim().length>0)
 const gc=(r,c)=>{try{
-  //P('gc: r='+r+' c='+c)
   let x=IN[r].charAt(c)
   if((typeof x === undefined)||(x===null)||(x===''))return '.'
   return x} catch(e){return '.'}}
@@ -14,18 +13,12 @@ const rays=(r,c)=>{
   let cnt=0
   for(let d=0;d<9;d++){
     let s=''
-    for(let n=0;n<4;n++){
-      s+=gc(r+(dr[d]*n),c+(dc[d]*n))
-      //P('---n='+n+' s='+s)
-    }
-    P('r='+r+' c='+c+' d='+d+' s='+s)
+    for(let n=0;n<4;n++)s+=gc(r+(dr[d]*n),c+(dc[d]*n))
     if(s==='XMAS')cnt++}
   return cnt}
 let xmas=0;
 for(let r=0;r<IN.length;r++){
-  P('IN['+r+']='+IN[r])
   for(let c=0;c<IN[r].length;c++){
-    //P('r='+r+' c='+c+' "'+gc(r,c)+'"')
     xmas+=rays(r,c)}}
 P('The answer is '+xmas+'.')
 
