cb704db260ea01db795b3a89337994423b2f92a9
Tom <tomangioletti@gmail.com>
2024-12-05 21:28:23 -0500

    Unit tester UT.js added.

---
+++ tom2024/UT.js
@@ -0,0 +1,7 @@
+const UT=(()=>{
+  let [f,p,s]=[0,0,'']
+  return (function(id,test){
+    if(arguments.length){if(test)p++; else s+=(f++>0?', ':' ')+id}
+    else return `${p} passed. ${f} failed. ${f?('Failures:'+s+'.'):''}`})
+  })()
+
